1254c7074c10e87ed6abc0e0858cf1a6c61f678d
Akmal Rahmonjonov <akmalsempai054@gmail.com>
2022-09-08 20:20:19 +0500

    Add theory abo ut ssh keys

---
+++ index.py
@@ -0,0 +1,11 @@
+# ssh-keygen -t rsa -b 4096 -C "akmalsempai@gmail.com
+
+# ssh key  generator
+# -t = type
+# -b = bits
+
+# ssh -T git@github.com
+# -T = tunnel connection to git@github.com
+
+# ! Public key = замочная скважина
+# ? Private key = ключ к замку
